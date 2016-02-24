a684244f8fe32f4808a56c88c4261945580ae78b
Randy Champagne <r-champagne@neiu.edu>
2016-02-24 10:24:40 -0500

    updated .gitignore to stop tracking useless dirs

--- RecoveryScriptBuilder/bin/RunScriptBuilder.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'ScriptBuilder'
-
-#project_number, cai_id, operation, input_file, output_file
-builder = ScriptBuilder.new(6998, 24572, "move_mp3",
-   "/Users/rchampa2/Desktop/Coursera/utils/RecoveryScriptBuilder/input/input.txt", 
-   "/Users/rchampa2/Desktop/Coursera/utils/RecoveryScriptBuilder/output/output.txt")
-
-builder.run()
